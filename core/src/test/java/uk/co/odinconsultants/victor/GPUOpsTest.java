0aefce6c80ef1b45bb4c262eac572bc71f1ee94a
PhillipHenry <PhillHenry@gmail.com>
2024-07-04 18:46:12 +0100

    Logging.

--- core/src/test/java/uk/co/odinconsultants/victor/GPUOpsTest.java
+++ core/src/test/java/uk/co/odinconsultants/victor/GPUOpsTest.java
@@ -1,11 +1,18 @@
 package uk.co.odinconsultants.victor;
 
+//import org.apache.logging.log4j.Level;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
 
 public class GPUOpsTest {
+    private final Logger logger = LogManager.getLogger();
+
     private final GPUOps toTest = new GPUOps();
 
     float x = 1;
@@ -29,14 +36,15 @@ public class GPUOpsTest {
     }
 
     @Test
-    @Ignore
     public void testTornadoDotProduct() {
         Assert.assertEquals(x * size, toTest.dot(vec, vec), 0f);
     }
 
     @Test
-    @Ignore
     public void testTornadoDotProductFloatrArray() {
+//        logger.log(Level.INFO, "Hello world");
+        logger.info("Hello world");
+        System.out.println("This is the console");
         Assert.assertEquals(x * size, toTest.dot(vecFloatArray, vecFloatArrayOther, size), 0f);
     }
 
