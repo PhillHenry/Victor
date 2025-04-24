65f7c0acb5aa5bb35d96af681d504b5ebcf09043
PhillHenry <PhillHenry@gmail.com>
2025-04-24 14:32:27 +0100

    Parallel at the column level. Quite a lot slower: Benchmark                    (arg)  Mode  Cnt        Score        Error  Units JMH_SoftMaxArray.softMaxGPU      8  avgt   15  1378504.747 ± 453393.453  ns/op

--- core/src/main/java/uk/co/odinconsultants/victor/SoftMax.java
+++ core/src/main/java/uk/co/odinconsultants/victor/SoftMax.java
@@ -9,7 +9,7 @@ import uk.ac.manchester.tornado.api.types.matrix.Matrix2DFloat;
 public class SoftMax {
 
     public static void sumArray(FloatArray m, @Reduce FloatArray result) {
-        for (@Parallel int row = 0; row < m.getElementSize() / result.getSize(); row++) {
+        for (int row = 0; row < m.getElementSize() / result.getSize(); row++) {
             for (@Parallel int col = 0; col < result.getSize(); col++) {
                 result.set(col, result.get(col) + m.get(row * result.getSize() + col));
             }
@@ -17,7 +17,7 @@ public class SoftMax {
     }
 
     public static void expInPlaceArray(FloatArray m, int nColumns) {
-        for (@Parallel int row = 0; row < m.getElementSize() / nColumns; row++) {
+        for (int row = 0; row < m.getElementSize() / nColumns; row++) {
             for (@Parallel int col = 0; col < nColumns; col++) {
                 m.set(row * nColumns + col, TornadoMath.exp(m.get(row * nColumns + col)));
             }
@@ -25,7 +25,7 @@ public class SoftMax {
     }
 
     public static void divideInPlaceArray(FloatArray m, @Reduce FloatArray d) {
-        for (@Parallel int row = 0; row < m.getElementSize() / d.getSize(); row++) {
+        for (int row = 0; row < m.getElementSize() / d.getSize(); row++) {
             for (@Parallel int col = 0; col < d.getSize(); col++) {
                 m.set(row * d.getSize() + col, m.get(row * d.getSize() + col) / d.get(col));
             }
