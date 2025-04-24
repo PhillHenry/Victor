339d638d4200be187e2517ecb6d4ea24239b2954
PhillHenry <PhillHenry@gmail.com>
2025-04-24 17:25:51 +0100

    Refactor. Benchmark                    (arg)  Mode  Cnt        Score       Error  Units JMH_SoftMaxArray.softMaxGPU      8  avgt   15  1025018.839 ± 31067.613  ns/op

--- benchmarking/src/test/java/uk/co/odinconsultants/victor/JMH_SoftMaxArray.java
+++ benchmarking/src/test/java/uk/co/odinconsultants/victor/JMH_SoftMaxArray.java
@@ -13,6 +13,8 @@ import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
 
 import java.util.concurrent.TimeUnit;
 
+import static uk.co.odinconsultants.victor.SoftMax.taskGraph;
+
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -25,7 +27,6 @@ public class JMH_SoftMaxArray {
 
     private final int n = (int)Math.pow(2, arg);
     private final FloatArray m = new FloatArray(n * n);
-    private TaskGraph t;
     TornadoExecutionPlan executor;
     FloatArray sum = new FloatArray(n);
 
@@ -34,14 +35,7 @@ public class JMH_SoftMaxArray {
         for (int i = 0; i < n * n; i++) {
             m.set(i, (float)Math.random());
         }
-        t = new TaskGraph("s0")
-                .transferToDevice(DataTransferMode.EVERY_EXECUTION, m, sum)
-                .task("t0", SoftMax::expInPlaceArray, m, n, n)
-                .task("t1", SoftMax::sumArray, m, sum, n, n)
-                .task("t2", SoftMax::divideInPlaceArray, m, sum, n, n)
-                .transferToHost(DataTransferMode.EVERY_EXECUTION, m);
-        ImmutableTaskGraph immutableTaskGraph = t.snapshot();
-        executor = new TornadoExecutionPlan(immutableTaskGraph);
+        executor = taskGraph(m, sum, n, n);
         executor.withWarmUp();
     }
 
--- core/src/main/java/uk/co/odinconsultants/victor/SoftMax.java
+++ core/src/main/java/uk/co/odinconsultants/victor/SoftMax.java
@@ -1,6 +1,10 @@
 package uk.co.odinconsultants.victor;
 
+import uk.ac.manchester.tornado.api.ImmutableTaskGraph;
+import uk.ac.manchester.tornado.api.TaskGraph;
+import uk.ac.manchester.tornado.api.TornadoExecutionPlan;
 import uk.ac.manchester.tornado.api.annotations.Parallel;
+import uk.ac.manchester.tornado.api.enums.DataTransferMode;
 import uk.ac.manchester.tornado.api.math.TornadoMath;
 import uk.ac.manchester.tornado.api.types.arrays.FloatArray;
 import uk.ac.manchester.tornado.api.types.matrix.Matrix2DFloat;
@@ -72,8 +76,31 @@ public class SoftMax {
         divideInPlace(m, sum);
     }
 
-    public static void main(String[] args) {
+    public static TornadoExecutionPlan taskGraph(FloatArray m, FloatArray sum, int nRows, int nCols) {
+        TaskGraph t = new TaskGraph("s0")
+                .transferToDevice(DataTransferMode.EVERY_EXECUTION, m, sum)
+                .task("t0", SoftMax::expInPlaceArray, m, nRows, nCols)
+                .task("t1", SoftMax::sumArray, m, sum, nRows, nCols)
+                .task("t2", SoftMax::divideInPlaceArray, m, sum, nRows, nCols)
+                .transferToHost(DataTransferMode.EVERY_EXECUTION, m);
+        ImmutableTaskGraph immutableTaskGraph = t.snapshot();
+        TornadoExecutionPlan executor = new TornadoExecutionPlan(immutableTaskGraph);
+        return executor;
+    }
 
+    public static void main(String[] args) {
+        int n = 1024;
+        final FloatArray m = new FloatArray(n * n);
+        final FloatArray sum = new FloatArray(n);
+        for (int i = 0; i < n * n; i++) {
+            m.set(i, 1f) ; //(float)Math.random());
+        }
+        TornadoExecutionPlan executor = taskGraph(m, sum, n, n);
+        executor.execute();
+        for (int i = 0; i < n; i++) {
+            System.out.print(m.get(i) + ", ");
+        }
+        System.out.println("Finished");
     }
 
 }
